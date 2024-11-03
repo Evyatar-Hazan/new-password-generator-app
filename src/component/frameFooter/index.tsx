bbbb5b1b5bbb22aab2c04da4ea0c629a1e9b9451
Evyatar Hazan <EvyatarHazan3.14@gmail.com>
2024-11-03 16:49:07 +0200

    fx footer

--- src/component/frameFooter/index.tsx
+++ src/component/frameFooter/index.tsx
@@ -64,7 +64,7 @@ const FrameFooter: React.FC<FrameFooterProps> = ({
   };
 
   return (
-      <Animated.View style={[styles.container, { opacity: opacityAnimation }]}>
+    <Animated.View style={[styles.container, { opacity: opacityAnimation }]}>
       <View style={styles.footerBackground}>
         <View style={styles.buttonsContainer}>
           {mapRenderButton.map((button, index) => renderButton(button, index))}
@@ -74,7 +74,6 @@ const FrameFooter: React.FC<FrameFooterProps> = ({
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
     position: 'relative',
@@ -82,7 +81,7 @@ const styles = StyleSheet.create({
   },
   footerBackground: {
     width: '100%',
-    height: 80,
+    height: 70,
     backgroundColor: '#B88AE8',
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
@@ -92,7 +91,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     alignItems: 'center',
     height: '100%',
-    paddingHorizontal: 20,
+    paddingHorizontal: 16,
   },
   button: {
     justifyContent: 'center',
@@ -102,31 +101,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   circleButton: {
-    top: -75,
-    width: 80,
-    height: 80,
-    borderRadius: 40,
+    top: -60,
+    width: 65,
+    height: 65,
+    borderRadius: 32.5,
     backgroundColor: '#6200EE',
     justifyContent: 'center',
     alignItems: 'center',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 8, // הגברת הצל לאפקט עגול
-    elevation: 8,
+    shadowOpacity: 0.2,
+    shadowRadius: 5,
+    elevation: 6,
     position: 'absolute',
   },
   oval: {
-    width: 110,
-    height: 50,
+    width: 90,
+    height: 45,
     backgroundColor: 'gray',
-    borderBottomLeftRadius: 80,
-    borderBottomRightRadius: 80,
+    borderBottomLeftRadius: 50,
+    borderBottomRightRadius: 50,
     position: 'absolute',
-    top: -40,
-    opacity: 0.7, // הגברת השקיפות לאפקט עדין יותר
+    top: -30,
+    opacity: 0.4,
   },
 });
-
 
 export default FrameFooter;
