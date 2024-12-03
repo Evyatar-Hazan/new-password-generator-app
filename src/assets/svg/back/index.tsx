1eb08e6dd57b7be8b2cd10ff0facd464b586a44f
Evyatar Hazan <EvyatarHazan3.14@gmail.com>
2024-12-03 19:55:39 +0200

    update back icon

--- src/assets/svg/back/index.tsx
+++ src/assets/svg/back/index.tsx
@@ -3,6 +3,7 @@ import {GestureResponderEvent, TouchableOpacity} from 'react-native';
 import {Path, Svg} from 'react-native-svg';
 import { useTheme } from '../../../themes/ThemeContext';
 import { themes } from '../../../themes/themes';
+import { useRTL } from '../../../i18n/RTLContext';
 
 interface BackIconProps {
   onPress?: (event: GestureResponderEvent) => void;
@@ -11,13 +12,19 @@ interface BackIconProps {
 const BackIcon: React.FC<BackIconProps> = ({ onPress }) => {
   const { theme } = useTheme();
   const colors = themes[theme];
+  const {isRTL} = useRTL();
   return (
     <TouchableOpacity onPress={onPress} style={{marginHorizontal: 15}}>
-      <Svg width={24} height={24} viewBox="0 0 24 24">
-        <Path fill={colors.text} d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
+      <Svg  width={24} height={24} viewBox="0 0 1024 1024"
+      style={{
+        transform: [{ scaleX: isRTL ? 1 : -1 }],
+      }}>
+        <Path fill={colors.text} 
+        d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64z"/>
+        <Path fill={colors.text} 
+        d="m237.248 512 265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312L237.248 512z"/>
       </Svg>
     </TouchableOpacity>
   );
 };
-
 export default BackIcon;
