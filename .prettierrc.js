4b5b12222a5cd7dbaa5eaf0a3b4619bbc53e6a4e
Evyatar Hazan <EvyatarHazan3.14@gmail.com>
2024-12-12 09:51:07 +0200

    add precommit

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  endOfLine: 'lf',
+  semi: true,
   singleQuote: true,
+  tabWidth: 2,
   trailingComma: 'all',
+  useTabs: false,
 };
