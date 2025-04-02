44ebaf9ec296abc9b4a089ca565d15094a4f39eb
Evyatar Hazan <EvyatarHazan3.14@gmail.com>
2025-04-02 17:44:07 +0300

    fix bug render

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import './i18n/i18n';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
 
 import SplashScreen from './component/splashScreen';
 import { RTLProvider } from './i18n/RTLContext';
@@ -22,16 +21,9 @@ const generateRandomString = (length: number): string => {
 };
 
 const App: React.FC = () => {
-  const { i18n } = useTranslation();
-  const [currentLanguage] = useState(i18n.language);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const changeLanguage = (nameLanguage: string) => {
-      i18n.changeLanguage(nameLanguage);
-    };
-    changeLanguage(currentLanguage);
-
     const loadRandomKey = async () => {
       const existingRandomKey = await AsyncStorage.getItem(randomKeyStorageKey);
 
@@ -49,7 +41,7 @@ const App: React.FC = () => {
     };
 
     loadApp();
-  }, [currentLanguage, i18n]);
+  }, []);
 
   return (
     <ThemeProvider>
